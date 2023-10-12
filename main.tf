2b07b341f6574965c20e70c0911791f76d9882d1
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-12 19:53:56 +0530

    subnet config delegation syntax change4

--- main.tf
+++ main.tf
@@ -68,8 +68,8 @@ module "subnet" {
         resource_group_name     =   "rg-ddi-poc"
         virtual_network_name    =   "vnet-ddi-poc"
         address_prefixes        =   ["10.100.0.0/24"]
-        service_endpoints       =   ["Microsoft.KeyVault"]
-        service_endpoint_policy_ids     =   ["/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourcegroups/rg-ddi-poc/providers/Microsoft.Network/serviceendpointpolicies/ddi-test-poc"]
+        service_endpoints       =   ["Microsoft.Storage"]
+        service_endpoint_policy_ids     =   "/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourcegroups/rg-ddi-poc/providers/Microsoft.Network/serviceendpointpolicies/ddi-test-poc"
         private_endpoint_network_polices_enabled       =   "false"
         private_link_service_network_policies_enabled   =   "false"
 
