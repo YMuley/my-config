80bfd2815075a3ab12fb57927c6324b84da874ff
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-12 18:53:01 +0530

    subnet config

--- main.tf
+++ main.tf
@@ -56,3 +56,32 @@ module "vnet" {
 }
 
 
+module "subnet" {
+  source  = "app.terraform.io/Motifworks/subnet/azurerm"
+  version = "1.0.0"
+  resource_group_output  = module.resource_Group.resource_group_output
+  virtual_network_output = module.vnet.virtual_network_output
+  
+  vnet_subnet_list  =   [
+    {
+        name = "sub-ddi-poc-web"
+        resource_group_name     =   "rg-ddi-poc"
+        virtual_network_name    =   "vnet-ddi-poc"
+        address_prefixes        =   ["10.100.0.0/24"]
+        service_endpoints       =   "Microsoft.KeyVault"
+        service_endpoint_policy_ids     =   []
+        private_endpoint_network_policies_enabled       =   "false"
+        private_link_service_network_policies_enabled   =   "false"
+
+        delegation = {
+            name    =  "delegation-container"
+                service_delegation ={
+                    name    =   "Microsoft.ContainerInstance/containerGroups"
+                    actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+                }
+        }
+
+    }
+
+  ]
+}
