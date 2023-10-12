6cedb0346a111c8c6da69c57512fb1e5f3b4af43
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-12 19:06:54 +0530

    subnet config delegation syntax change

--- main.tf
+++ main.tf
@@ -74,9 +74,9 @@ module "subnet" {
         private_link_service_network_policies_enabled   =   "false"
 
         delegation = {
-            name    =  "delegation_container"
+            name    =  "delegation"
                 service_delegation ={
-                    name    =   "Microsoft.ContainerInstance/containerGroups"
+                    name    =   ["Microsoft.ContainerInstance/containerGroups"]
                     actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
                 }
         }
