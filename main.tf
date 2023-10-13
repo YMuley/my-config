712e3f98403da85aa468adf6564236c192baaca0
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-13 16:24:02 +0530

    new commit2

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ module "resource_Group" {
   source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
   version = "1.0.2"
     resource_group_list = [
-        {
-            name        = "rg-ddi-poc" 
-            location    = "eastus"
-            tags        = {
-                location      = "eastus"
-                subscription  = "iac-dev"
-                environment   = "poc"
-            }
-        },
+        # {
+        #     name        = "rg-ddi-poc" 
+        #     location    = "eastus"
+        #     tags        = {
+        #         location      = "eastus"
+        #         subscription  = "iac-dev"
+        #         environment   = "poc"
+        #     }
+        # },
         # {
         #     name        = "rg-ddi-dev" 
         #     location    = "westus"
