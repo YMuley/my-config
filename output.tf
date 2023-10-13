f632ad7982e9c2056f8636d1e47959b109dbfffb
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-13 23:44:38 +0530

    first commit

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "resource_group_output" {
-   value = module.resource_Group.resource_group_output
+   value = module.resource_Group.value
   }
 
 output "virtual_network_output" {
-  value = module.vnet.virtual_network_output
+  value = module.vnet.value
 }
