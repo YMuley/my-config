c2d9b0551fd054bfc0d8f8e8787872d2082bd578
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-19 19:30:34 +0530

    file update

--- main.tf
+++ main.tf
@@ -39,17 +39,17 @@ module "vnet" {
             environment = "poc"
         }
     },
-    # {
-    #     name        = "vnet-ddi-dev"
-    #     location    = "westus"
-    #     resource_group_name = "rg-ddi-dev"
-    #     address_space = ["10.100.50.0/24"] //["172.21.0.0/16"]
-    #     dns_server = [] //["172.21.1.40", "172.21.1.41"]
-    #     tags = {
-    #         environment = "poc"
-    #     }
+    {
+        name        = "vnet-ddi-dev"
+        location    = "westus"
+        resource_group_name = "rg-ddi-dev"
+        address_space = ["10.100.50.0/24"] //["172.21.0.0/16"]
+        dns_server = [] //["172.21.1.40", "172.21.1.41"]
+        tags = {
+            environment = "poc"
+        }
 
-    # }
+    }
     ]
 
 }
@@ -58,34 +58,86 @@ module "vnet" {
 module "subnet" {
   source  = "app.terraform.io/Motifworks/subnet/azurerm"
   version = "1.0.0"
-  #resource_group_output  = module.resource_Group.resource_group_output
-  #virtual_network_output = module.vnet.virtual_network_output
+  resource_group_output  = module.resource_Group.resource_group_output
+  virtual_network_output = module.vnet.virtual_network_output
+  service_enpoint_policy_output = module.service_endpoint_policy.service_endpoint_policy_output
   
   vnet_subnet_list  =   [
-    # {
-    #     name = "sub-ddi-poc-web"
-    #     resource_group_name     =   "rg-ddi-poc"
-    #     virtual_network_name    =   "vnet-ddi-poc"
-    #     address_prefixes        =   ["10.100.0.0/24"]
-    #     service_endpoints       =   ["Microsoft.Storage"]
-    #     service_endpoint_policy_ids     =   ["/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourceGroups/rg-ddi-poc/providers/Microsoft.Network/serviceEndpointPolicies/ddi-test-poc/"]
-    #     private_endpoint_network_polices_enabled       =   "true"
-    #     private_link_service_network_policies_enabled   =   "true"
+    {
+        name = "sub-ddi-poc-web"
+        resource_group_name     =   "rg-ddi-poc"
+        virtual_network_name    =   "vnet-ddi-poc"
+        address_prefixes        =   ["10.100.0.0/24"]
+        service_endpoints       =   ["Microsoft.Storage"]
+        service_endpoint_policy_ids     =  ["ddi-sep-poc"] #["/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourceGroups/rg-ddi-poc/providers/Microsoft.Network/serviceEndpointPolicies/ddi-test-poc/"]
+        private_endpoint_network_polices_enabled       =   "true"
+        private_link_service_network_policies_enabled   =   "true"
 
-    #     delegation = [
-    #         {
-    #         name    =  "delegation"
-    #             service_delegation = [ 
-    #                 {
-    #                 name    =   "Microsoft.ContainerInstance/containerGroups"
-    #                 actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+        delegation = [
+            {
+            name    =  "delegation"
+                service_delegation = [ 
+                    {
+                    name    =   "Microsoft.ContainerInstance/containerGroups"
+                    actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
                 
-    #     } 
-    #     ]
+        } 
+        ]
 
-    # } 
-    # ]
-    # }
+    } 
+    ]
+    },
+
+     {
+        name = "sub-ddi-dev-web"
+        resource_group_name     =   "rg-ddi-dev"
+        virtual_network_name    =   "vnet-ddi-dev"
+        address_prefixes        =   ["10.100.0.0/24"]
+        service_endpoints       =   []
+        service_endpoint_policy_ids     =   []
+        private_endpoint_network_polices_enabled       =   "false"
+        private_link_service_network_policies_enabled   =   "false"
+
+        delegation = [
+            {
+            name    =  "delegation"
+                service_delegation = [ 
+                    {
+                    name    =   "Microsoft.ContainerInstance/containerGroups"
+                    actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+                
+        } 
+        ]
+
+    } 
+    ]
+    }
 
   ]
+}
+
+module "service_endpoint_policy" {
+  source  = "app.terraform.io/Motifworks/service_endpoint_policy/azurerm"
+  version = "1.0.0"
+   resource_group_output = module.resource_Group.resource_group_output
+  service_endpoint_policy_list = [
+    {
+      name                  = "ddi-sep-poc"
+      resource_group_name   = "rg-ddi-poc"
+      location              = "eastus"
+
+      defination = [
+        {
+          name              = "spe-stg-ddi-poc"
+          description       = "poc policy"
+          service           = "Microsoft.Storage"
+          service_resources =  [ module.resource_Group.resource_group_output["rg-ddi-poc"].name ]
+
+        }
+        
+        ]
+    }
+  ]
+  
+
 }
