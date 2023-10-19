8a093d8c88fe3a2dd0a48dd20bed2cb67bd69846
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-19 20:31:11 +0530

    subnet configupdate

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "resource_group_output" {
 output "virtual_network_output" {
   value = module.vnet.virtual_network_output
 }
+
+output "service_endpoint_policy_output" {
+  value = module.service_endpoint_policy.service_endpoint_policy_output
+}
