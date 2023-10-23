d5d2b3378d6ac08e20214665c666669a4eccc323
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-23 14:40:50 +0530

    config uncommneted

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ module "resource_Group" {
   source  = "app.terraform.io/Motifworks/resource_Group/azurerm"
   version = "1.0.2"
   resource_group_list = [
-    # {
-    #   name     = "rg-ddi-poc"
-    #   location = "eastus"
-    #   tags = {
-    #     location     = "eastus"
-    #     subscription = "iac-dev"
-    #     environment  = "poc"
-    #   }
-    # },
-    # {
-    #   name     = "rg-ddi-dev"
-    #   location = "westus"
-    #   tags = {
-    #     location     = "westus"
-    #     subscription = "iac-dev"
-    #     environment  = "dev"
-    #   }
-    # }
+    {
+      name     = "rg-ddi-poc"
+      location = "eastus"
+      tags = {
+        location     = "eastus"
+        subscription = "iac-dev"
+        environment  = "poc"
+      }
+    },
+    {
+      name     = "rg-ddi-dev"
+      location = "westus"
+      tags = {
+        location     = "westus"
+        subscription = "iac-dev"
+        environment  = "dev"
+      }
+    }
   ]
 }
 
@@ -29,26 +29,26 @@ module "vnet" {
   version               = "1.0.0"
   resource_group_output = module.resource_Group.resource_group_output
   virtual_network_list = [
-    # {
-    #   name                = "vnet-ddi-poc"
-    #   location            = "eastus"
-    #   resource_group_name = "rg-ddi-poc"
-    #   address_space       = ["10.100.0.0/16"] //["172.21.0.0/16"]
-    #   dns_server          = []                //["172.21.1.40", "172.21.1.41"]
-    #   tags = {
-    #     environment = "poc"
-    #   }
-    # },
-    # {
-    #   name                = "vnet-ddi-dev"
-    #   location            = "westus"
-    #   resource_group_name = "rg-ddi-dev"
-    #   address_space       = ["10.100.50.0/24"] //["172.21.0.0/16"]
-    #   dns_server          = []                 //["172.21.1.40", "172.21.1.41"]
-    #   tags = {
-    #     environment = "poc"
-    #   }
-    # }
+    {
+      name                = "vnet-ddi-poc"
+      location            = "eastus"
+      resource_group_name = "rg-ddi-poc"
+      address_space       = ["10.100.0.0/16"] //["172.21.0.0/16"]
+      dns_server          = []                //["172.21.1.40", "172.21.1.41"]
+      tags = {
+        environment = "poc"
+      }
+    },
+    {
+      name                = "vnet-ddi-dev"
+      location            = "westus"
+      resource_group_name = "rg-ddi-dev"
+      address_space       = ["10.100.50.0/24"] //["172.21.0.0/16"]
+      dns_server          = []                 //["172.21.1.40", "172.21.1.41"]
+      tags = {
+        environment = "poc"
+      }
+    }
   ]
 
 }
@@ -62,55 +62,55 @@ module "subnet" {
   service_endpoint_policy_output = module.service_endpoint_policy.service_endpoint_policy_output
 
   vnet_subnet_list = [
-  #   {
-  #       name = "sub-ddi-poc-web"
-  #       resource_group_name     =   "rg-ddi-poc"
-  #       virtual_network_name    =   "vnet-ddi-poc"
-  #       address_prefixes        =   ["10.100.0.0/24"]
-  #       service_endpoints       =   ["Microsoft.Storage"]
-  #       service_endpoint_policy_ids     = ["ddi-sep-poc"] #["/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourceGroups/rg-ddi-poc/providers/Microsoft.Network/serviceEndpointPolicies/ddi-test-poc/"]
-  #       private_endpoint_network_polices_enabled       =   "true"
-  #       private_link_service_network_policies_enabled   =   "true"
+    {
+        name = "sub-ddi-poc-web"
+        resource_group_name     =   "rg-ddi-poc"
+        virtual_network_name    =   "vnet-ddi-poc"
+        address_prefixes        =   ["10.100.0.0/24"]
+        service_endpoints       =   ["Microsoft.Storage"]
+        service_endpoint_policy_ids     = ["ddi-sep-poc"] #["/subscriptions/8694217e-4a30-4107-9a12-aeac74b82f5c/resourceGroups/rg-ddi-poc/providers/Microsoft.Network/serviceEndpointPolicies/ddi-test-poc/"]
+        private_endpoint_network_polices_enabled       =   "true"
+        private_link_service_network_policies_enabled   =   "true"
 
-  #     delegation = [
-  #         {
-  #         name    =  "delegation"
-  #             service_delegation = [ 
-  #                 {
-  #                 name    =   "Microsoft.ContainerInstance/containerGroups"
-  #                 actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+      delegation = [
+          {
+          name    =  "delegation"
+              service_delegation = [ 
+                  {
+                  name    =   "Microsoft.ContainerInstance/containerGroups"
+                  actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
 
-  #     } 
-  #     ]
+      } 
+      ]
 
-  # } 
-  # ]
-  # },
+  } 
+  ]
+  },
 
-  #    {
-  #       name = "sub-ddi-dev-web"
-  #       resource_group_name     =   "rg-ddi-dev"
-  #       virtual_network_name    =   "vnet-ddi-dev"
-  #       address_prefixes        =   ["10.100.50.0/24"]
-  #       service_endpoints       =   []
-  #       service_endpoint_policy_ids     = ["ddi-sep-poc"]
-  #       private_endpoint_network_polices_enabled       =   "false"
-  #       private_link_service_network_policies_enabled   =   "false"
+     {
+        name = "sub-ddi-dev-web"
+        resource_group_name     =   "rg-ddi-dev"
+        virtual_network_name    =   "vnet-ddi-dev"
+        address_prefixes        =   ["10.100.50.0/24"]
+        service_endpoints       =   [""]
+        service_endpoint_policy_ids     = [""]
+        private_endpoint_network_polices_enabled       =   "false"
+        private_link_service_network_policies_enabled   =   "false"
 
-  #       delegation = [
-  #           {
-  #           name    =  "delegation"
-  #               service_delegation = [ 
-  #                   {
-  #                   name    =   "Microsoft.ContainerInstance/containerGroups"
-  #                   actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+        delegation = [
+            {
+            name    =  "delegation"
+                service_delegation = [ 
+                    {
+                    name    =   "Microsoft.ContainerInstance/containerGroups"
+                    actions =   ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
 
-  #       } 
-  #       ]
+        } 
+        ]
 
-  #   } 
-  #   ]
-  #   }
+    } 
+    ]
+    }
 
   ]
 }
@@ -120,22 +120,22 @@ module "service_endpoint_policy" {
   version               = "1.0.0"
   resource_group_output = module.resource_Group.resource_group_output
   service_endpoint_policy_list = [
-    # {
-    #   name                = "ddi-sep-poc"
-    #   resource_group_name = "rg-ddi-poc"
-    #   location            = "eastus"
+    {
+      name                = "ddi-sep-poc"
+      resource_group_name = "rg-ddi-poc"
+      location            = "eastus"
 
-    #   definition = [
-    #     {
-    #       name              = "spe-stg-ddi-poc"
-    #       description       = "poc policy"
-    #       service           = "Microsoft.Storage"
-    #       service_resources = [module.resource_Group.resource_group_output["rg-ddi-poc"].id]
+      definition = [
+        {
+          name              = "spe-stg-ddi-poc"
+          description       = "poc policy"
+          service           = "Microsoft.Storage"
+          service_resources = [module.resource_Group.resource_group_output["rg-ddi-poc"].id]
 
-    #     }
+        }
 
-    #   ]
-    # }
+      ]
+    }
   ]
 
 
@@ -144,65 +144,65 @@ module "service_endpoint_policy" {
 module "network_security_group" {
   source  = "app.terraform.io/Motifworks/network_security_group/azurerm"
   version = "1.0.0"
-  #   resource_group_output = module.resource_Group.resource_group_output
-  #   network_security_group_list = [
-  #     {
-  #       name                = "nsg-ddi-poc"
-  #       location            = "eastus"
-  #       resource_group_name = "rg-ddi-poc"
-  #       tags = {
-  #         env = "poc"
-  #       }
-  #       security_rule = [
-  #         {
-  #           name                       = "HTTP"
-  #           priority                   = 1001
-  #           direction                  = "Inbound"
-  #           access                     = "Allow"
-  #           protocol                   = "Tcp"
-  #           source_port_range          = "*"
-  #           destination_port_range     = "80"
-  #           source_address_prefix      = "*"
-  #           destination_address_prefix = "*"
-  #         },
-  #         {
-  #           name                       = "HTTPS"
-  #           priority                   = 1002
-  #           direction                  = "Inbound"
-  #           access                     = "Allow"
-  #           protocol                   = "Tcp"
-  #           source_port_range          = "*"
-  #           destination_port_range     = "443"
-  #           source_address_prefix      = "*"
-  #           destination_address_prefix = "*"
-  #         }
-  #       ]
+    resource_group_output = module.resource_Group.resource_group_output
+    network_security_group_list = [
+      {
+        name                = "nsg-ddi-poc"
+        location            = "eastus"
+        resource_group_name = "rg-ddi-poc"
+        tags = {
+          env = "poc"
+        }
+        security_rule = [
+          {
+            name                       = "HTTP"
+            priority                   = 1001
+            direction                  = "Inbound"
+            access                     = "Allow"
+            protocol                   = "Tcp"
+            source_port_range          = "*"
+            destination_port_range     = "80"
+            source_address_prefix      = "*"
+            destination_address_prefix = "*"
+          },
+          {
+            name                       = "HTTPS"
+            priority                   = 1002
+            direction                  = "Inbound"
+            access                     = "Allow"
+            protocol                   = "Tcp"
+            source_port_range          = "*"
+            destination_port_range     = "443"
+            source_address_prefix      = "*"
+            destination_address_prefix = "*"
+          }
+        ]
 
-  #     },
-  #     {
-  #       name                = "nsg-ddi-poc-one"
-  #       location            = "eastus"
-  #       resource_group_name = "rg-ddi-poc"
-  #       tags = {
-  #         env = "poc"
-  #       }
-  #       security_rule = [
-  #         {
-  #           name                       = "HTTP"
-  #           priority                   = 1001
-  #           direction                  = "Inbound"
-  #           access                     = "Allow"
-  #           protocol                   = "Tcp"
-  #           source_port_range          = "*"
-  #           destination_port_range     = "80"
-  #           source_address_prefix      = "*"
-  #           destination_address_prefix = "*"
-  #         }
+      },
+      {
+        name                = "nsg-ddi-poc-one"
+        location            = "eastus"
+        resource_group_name = "rg-ddi-poc"
+        tags = {
+          env = "poc"
+        }
+        security_rule = [
+          {
+            name                       = "HTTP"
+            priority                   = 1001
+            direction                  = "Inbound"
+            access                     = "Allow"
+            protocol                   = "Tcp"
+            source_port_range          = "*"
+            destination_port_range     = "80"
+            source_address_prefix      = "*"
+            destination_address_prefix = "*"
+          }
 
-  #       ]
+        ]
 
 
-  #   }]
+    }]
 }
 
 
@@ -222,34 +222,34 @@ module "vnet_dns" {
 module "public-ip" {
   source  = "app.terraform.io/Motifworks/public-ip/azurerm"
   version = "1.0.0"
-  #   resource_group_output = module.resource_Group.resource_group_output
+    resource_group_output = module.resource_Group.resource_group_output
 
-  #   public_ip_list = [
-  #     {
-  #       name                = "publicip-ddi-poc"
-  #       location            = "eastus"
-  #       resource_group_name = "rg-ddi-poc"
-  #       allocation_method   = "Static"
-  #       sku                 = "Basic"
-  #       domain_name_label   = "unique-testing-label"  
-  #       tags = {
-  #         environment = "poc"
-  #       }
-  #       sku_tier = "Regional"
-  #     },
-  #     {
-  #       name                = "publicip-ddi-dev"
-  #       location            = "westus"
-  #       resource_group_name = "rg-ddi-dev"
-  #       allocation_method   = "Static" 
-  #       sku                 = "Basic"
-  #       domain_name_label   = "another-unique-label" 
-  #       tags = {
-  #         environment = "dev"
-  #       }
-  #       sku_tier = "Global"
-  #     }
-  #   ]
+    public_ip_list = [
+      {
+        name                = "publicip-ddi-poc"
+        location            = "eastus"
+        resource_group_name = "rg-ddi-poc"
+        allocation_method   = "Static"
+        sku                 = "Basic"
+        domain_name_label   = "unique-testing-label"  
+        tags = {
+          environment = "poc"
+        }
+        sku_tier = "Regional"
+      },
+      {
+        name                = "publicip-ddi-dev"
+        location            = "westus"
+        resource_group_name = "rg-ddi-dev"
+        allocation_method   = "Static" 
+        sku                 = "Basic"
+        domain_name_label   = "another-unique-label" 
+        tags = {
+          environment = "dev"
+        }
+        sku_tier = "Global"
+      }
+    ]
 }
 
 module "route-table" {
@@ -258,30 +258,30 @@ module "route-table" {
   resource_group_output = module.resource_Group.resource_group_output
 
   route_table_list = [
-    # {
-    #   name                = "rt-table1"
-    #   location            = "eastus"
-    #   resource_group_name = "rg-ddi-dev"
-    #   tags = {
-    #     environment = "poc"
-    #     application = "example"
-    #   }
+    {
+      name                = "rt-table1"
+      location            = "eastus"
+      resource_group_name = "rg-ddi-dev"
+      tags = {
+        environment = "poc"
+        application = "example"
+      }
       
-    #   disable_bgp_route_propagation = true
-    #   route_list = [
-    #     {
-    #       name                   = "route1"
-    #       address_prefix         = "10.0.0.0/16"
-    #       next_hop_type          = "VirtualAppliance"
-    #       next_hop_in_ip_address = "10.0.0.1"
-    #     },
-    #     {
-    #       name                   = "route2"
-    #       address_prefix         = "10.1.0.0/16"
-    #       next_hop_type          = "VirtualAppliance"
-    #       next_hop_in_ip_address = "10.0.0.2"
-    #     }
-    #   ]
-    # }
+      disable_bgp_route_propagation = true
+      route_list = [
+        {
+          name                   = "route1"
+          address_prefix         = "10.0.0.0/16"
+          next_hop_type          = "VirtualAppliance"
+          next_hop_in_ip_address = "10.0.0.1"
+        },
+        {
+          name                   = "route2"
+          address_prefix         = "10.1.0.0/16"
+          next_hop_type          = "VirtualAppliance"
+          next_hop_in_ip_address = "10.0.0.2"
+        }
+      ]
+    }
   ]
 }
