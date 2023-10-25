4dcff675edbb74abf6f706b3b6fdaa0a6552e43d
sunandasharma61 <sunandasharma60101@gmail.com>
2023-10-25 11:58:05 +0530

    testing

--- main.tf
+++ main.tf
@@ -294,6 +294,52 @@ module "route-table" {
 }
 
 
+# module "network_interface_card" {
+#   source                = "app.terraform.io/Motifworks/network_interface_card/azurerm"
+#   version               = "1.0.0"
+#   resource_group_output = module.resource_Group.resource_group_output
+
+#   network_interface_card_list = [
+#     {
+#       name                = "nic1"
+#       location            = "eastus"
+#       resource_group_name = "rg-ddi-poc"
+#       tags = {
+#         environment = "poc"
+#       }
+#       ip_configuration = [
+#         {
+#           name                          = "config1"
+#           virtual_network_name          = "vnet-ddi-poc"
+#           subnet_name                   = ["sub-ddi-poc-web"]
+#           private_ip_address_allocation = "dynamic"
+#           public_ip_name                = ["public-ip1"]
+#           private_ip_address            = null
+#         }
+#       ]
+#     },
+#     {
+#       name                = "nic2"
+#       location            = "westus"
+#       resource_group_name = "rg-ddi-dev"
+#       tags = {
+#         environment = "dev"
+#       }
+#       ip_configuration = [
+#         {
+#           name                          = "config2"
+#           virtual_network_name          = "vnet-ddi-dev"
+#           subnet_name                   = ["sub-ddi-dev-web"]
+#           private_ip_address_allocation = "dynamic"
+#           public_ip_name                = ["public-ip2"]
+#           private_ip_address            = null
+#         }
+#       ]
+#     }
+#   ]
+# }
+
+
 module "network_interface_card" {
   source                = "app.terraform.io/Motifworks/network_interface_card/azurerm"
   version               = "1.0.0"
@@ -311,9 +357,9 @@ module "network_interface_card" {
         {
           name                          = "config1"
           virtual_network_name          = "vnet-ddi-poc"
-          subnet_name                   = ["sub-ddi-poc-web"]
-          private_ip_address_allocation = "dynamic"
-          public_ip_name                = ["public-ip1"]
+          subnet_name                   = "sub-ddi-poc-web"
+          private_ip_address_allocation = "Dynamic"
+          public_ip_name                = "public-ip1"
           private_ip_address            = null
         }
       ]
@@ -329,15 +375,13 @@ module "network_interface_card" {
         {
           name                          = "config2"
           virtual_network_name          = "vnet-ddi-dev"
-          subnet_name                   = ["sub-ddi-dev-web"]
-          private_ip_address_allocation = "dynamic"
-          public_ip_name                = ["public-ip2"]
+          subnet_name                   = "sub-ddi-dev-web"
+          private_ip_address_allocation = "Dynamic"
+          public_ip_name                = "public-ip2"
           private_ip_address            = null
         }
       ]
     }
   ]
 }
-
-
 
