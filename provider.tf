41d6ed9a4f309fb5710056cc37ad9bd0dd01dab1
sunandasharma61 <sunandasharma60101@gmail.com>
2023-10-30 11:24:24 +0530

    testing

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 terraform {
-   #required_version = ">= 1.5.7"//0.13.3
+  #required_version = ">= 1.5.7"//0.13.3
   required_providers {
     azurerm = {
       version = ">=3.75.0"
@@ -9,11 +9,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {  
+  features {
     resource_group {
       prevent_deletion_if_contains_resources = true
     }
-    }
+  }
   skip_provider_registration = true
- 
+
 }
