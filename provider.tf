f26e579383970db407d995f37d0ffa69c6b570dc
sunandasharma61 <sunandasharma60101@gmail.com>
2023-10-30 13:03:48 +0530

    testing

--- provider.tf
+++ provider.tf
@@ -10,10 +10,14 @@ terraform {
 
 provider "azurerm" {
   features {
+
     resource_group {
       prevent_deletion_if_contains_resources = true
+
     }
+
   }
   skip_provider_registration = true
+  storage_use_azuread        = false
 
 }
