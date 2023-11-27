b72a58476907f3f6baaed35f9d3386b3c341e4be
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-11-27 20:03:16 +0530

    config updated

--- main.tf
+++ main.tf
@@ -1121,7 +1121,7 @@ module "loadbalancer_outbound_rule" {
   load_distribution = "Default"  # possible values [Default ,SourceIP, SourceIPProtocol, None ,Client IP, Client IP and Protocol]
   disable_outbound_snat = false
   enable_tcp_reset = false
-  backend_address_pool_names = ["bkp-lb-ddi-dev" , "bkp-lb-ddi-dev1"]
+  backend_address_pool_names = flatten(["bkp-lb-ddi-dev" , "bkp-lb-ddi-dev1"])
     }
   ]
    depends_on = [ module.load_balancer, module.loadbalancer_backend_pool, module.loadbalancer_health_probe ]
