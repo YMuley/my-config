7476fa07e13a5011691347e544f954178e9bca52
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-12-04 18:09:08 +0530

    config update

--- main.tf
+++ main.tf
@@ -1017,7 +1017,7 @@ module "loadbalancer_backend_pool" {
           identifier = "801"
           type       = "External"
           protocol   = "VXLAN"
-          port       = "443"
+          port       = "8080"
         }
       ]
     }
