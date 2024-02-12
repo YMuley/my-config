f8250fdf992e211be4729129b3e3785c64c0cf94
sunanda sharma <sunandasharma@ddiworld.com>
2024-02-12 13:40:12 +0530

    config updated

--- main.tf
+++ main.tf
@@ -389,24 +389,24 @@ module "subnet" {
       delegation = []
     },
 
-    {
-      name                                          = "GatewaySubnet"
-      resource_group_name                           = "rg-ddi-dev1"
-      virtual_network_name                          = "vnet-ddi-dev1"
-      address_prefixes                              = ["10.100.17.0/24"]
-      service_endpoints                             = []
-      service_endpoint_policy_ids                   = []
-      private_endpoint_network_polices_enabled      = "false"
-      private_link_service_network_policies_enabled = "false"
+    # {
+    #   name                                          = "GatewaySubnet"
+    #   resource_group_name                           = "rg-ddi-dev1"
+    #   virtual_network_name                          = "vnet-ddi-dev1"
+    #   address_prefixes                              = ["10.100.17.0/24"]
+    #   service_endpoints                             = []
+    #   service_endpoint_policy_ids                   = []
+    #   private_endpoint_network_polices_enabled      = "false"
+    #   private_link_service_network_policies_enabled = "false"
 
-      delegation = []
-    },
+    #   delegation = []
+    # },
 
     {
       name                                          = "AzureFirewallSubnet"
       resource_group_name                           = "rg-ddi-dev1"
       virtual_network_name                          = "vnet-ddi-dev1"
-      address_prefixes                              = ["10.100.16.0/22"]
+      address_prefixes                              = ["10.100.17.0/24"]
       service_endpoints                             = []
       service_endpoint_policy_ids                   = []
       private_endpoint_network_polices_enabled      = "false"
