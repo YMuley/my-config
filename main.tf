41e86f4de7487eabdcceda79b2ea793de5dd63f9
sunanda sharma <sunandasharma@ddiworld.com>
2024-02-12 12:29:32 +0530

    config updated

--- main.tf
+++ main.tf
@@ -400,7 +400,21 @@ module "subnet" {
       private_link_service_network_policies_enabled = "false"
 
       delegation = []
+    },
+
+    {
+      name                                          = "AzureFirewallSubnet"
+      resource_group_name                           = "rg-ddi-dev1"
+      virtual_network_name                          = "vnet-ddi-dev1"
+      address_prefixes                              = ["10.100.4.0/24"]
+      service_endpoints                             = []
+      service_endpoint_policy_ids                   = []
+      private_endpoint_network_polices_enabled      = "false"
+      private_link_service_network_policies_enabled = "false"
+
+      delegation = []
     }
+
 
   ]
   depends_on = [module.virtual_network]
@@ -1409,6 +1423,40 @@ module "private_link_service" {
         virtual_network_name       = "vnet-ddi-dev1"
         subnet_name                = "sub-ddi-dev-web"
       }]
+    }
+  ]
+}
+
+module "firewall" {
+  source                = "app.terraform.io/Motifworks/firewall/azurerm"
+  version               = "1.0.0"
+  resource_group_output = module.resource_Group.resource_group_output
+  subnet_output         = module.subnet.vnet_subnet_output
+
+  azure_firewall_list = [
+    {
+      name                = "firewall1"
+      resource_group_name = "rg-firewall-dev"
+      location            = "westus"
+      sku_name            = "AZFW_VNet"
+      sku_tier            = "Standard"
+      dns_servers         = ["168.63.129.16"]
+      private_ip_ranges   = ["10.0.0.0/8"]
+      zones               = ["1"]
+      threat_intel_mode   = "Alert"
+
+      ip_configuration = [
+        {
+          name                 = "ip-config-1"
+          virtual_network_name = "vnet-firewall-dev"
+          subnet_name          = "AzureFirewallSubnet"
+          public_ip_name       = "public-ip-firewall"
+        }
+      ]
+
+      tags = {
+        environment = "dev"
+      }
     }
   ]
 }
