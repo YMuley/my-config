26b66c67e261b2eed27d0705a5f74ebd169e1be7
sunanda sharma <sunandasharma@ddiworld.com>
2024-02-20 16:52:04 +0530

    config updated

--- main.tf
+++ main.tf
@@ -1546,7 +1546,7 @@ module "firewall_nat_rule_collection" {
           destination_addresses = ["13.64.185.193"]
           destination_ports     = ["80", "443"]
           translated_address    = "192.168.1.1"
-          translated_port       = "8000-9000"
+          translated_port       = 443
           protocols             = ["TCP"]
         }
       ]
