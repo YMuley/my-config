c7c8d340138ca31c82384495b2814dbdb2425fd5
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2024-02-20 22:13:14 +0530

    change in config

--- main.tf
+++ main.tf
@@ -1460,9 +1460,7 @@ module "private_link_service" {
       resource_group_name              = "rg-ddi-dev1"
       location                         = "westus"
       lb_frontend_ip_configuration     = [{name = "lb-ddi-dev"
-                                            index = "0"},
-                                            {name = "lb-ddi-dev"
-                                             index = "1"}] ##[module.load_balancer.load_balancer_output["lb-ddi-dev"].frontend_ip_configuration[0].id]
+                                            index = "0"}] ##[module.load_balancer.load_balancer_output["lb-ddi-dev"].frontend_ip_configuration[0].id]
       tags = {
         environment = "dev"
       }
