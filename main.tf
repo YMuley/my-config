8de45323cd19077e3af92caa3645118e6ecc6b4b
sunanda sharma <sunandasharma@ddiworld.com>
2024-03-12 14:20:40 +0530

    config updated

--- main.tf
+++ main.tf
@@ -887,20 +887,20 @@ module "keyvault" {
 
 }
 
-module "vault_secret" {
-  source           = "app.terraform.io/Motifworks/vault_secret/key"
-  version          = "1.0.0"
-  key_vault_output = module.keyvault.key_vault_output
-  key_vault_secret_list = [
-    {
-      name           = "secrauce"
-      value          = "szechuan"
-      key_vault_name = "testiefngkvrss2"
+# module "vault_secret" {
+#   source           = "app.terraform.io/Motifworks/vault_secret/key"
+#   version          = "1.0.0"
+#   key_vault_output = module.keyvault.key_vault_output
+#   key_vault_secret_list = [
+#     {
+#       name           = "secrauce"
+#       value          = "szechuan"
+#       key_vault_name = "testiefngkvrss2"
 
-    }
-  ]
-  depends_on = [module.keyvault]
-}
+#     }
+#   ]
+#   depends_on = [module.keyvault]
+# }
 
 
 module "storage_account" {
