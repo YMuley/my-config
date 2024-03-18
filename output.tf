97c6351306d612c5d7f89a630c9738255b10d718
sunanda sharma <sunandasharma@ddiworld.com>
2024-03-18 16:57:24 +0530

    config updated

--- output.tf
+++ output.tf
@@ -6,33 +6,33 @@ output "virtual_network_output" {
   value = module.virtual_network.virtual_network_output
 }
 
-output "service_endpoint_policy_output" {
-  value = module.service_endpoint_policy.service_endpoint_policy_output
-}
+# output "service_endpoint_policy_output" {
+#   value = module.service_endpoint_policy.service_endpoint_policy_output
+# }
 
-output "vnet_subnet_output" {
-  value = module.subnet.vnet_subnet_output
-}
+# output "vnet_subnet_output" {
+#   value = module.subnet.vnet_subnet_output
+# }
 
-output "snet_network_security_group_output" {
-  value = module.network_security_group.network_security_group_output
-}
+# output "snet_network_security_group_output" {
+#   value = module.network_security_group.network_security_group_output
+# }
 
-output "route_table_output" {
-  value = module.route_table.route_table_output
-}
+# output "route_table_output" {
+#   value = module.route_table.route_table_output
+# }
 
-output "virtual_network_dns_output" {
-  value = module.virtual_network_dns.virtual_network_dns_output
-}
+# output "virtual_network_dns_output" {
+#   value = module.virtual_network_dns.virtual_network_dns_output
+# }
 
 output "public_ip_output" {
   value = module.public_ip.public_ip_output
 }
 
-output "network_interface_card_output" {
-  value = module.network_interface_card.network_interface_card_output
-}
+# output "network_interface_card_output" {
+#   value = module.network_interface_card.network_interface_card_output
+# }
 
 output "azure_firewall_output" {
   value = module.azure_firewall.azure_firewall_output
