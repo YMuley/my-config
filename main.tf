246585651476b2297717c5ac26beddd7d143e526
sunanda sharma <sunandasharma@ddiworld.com>
2024-03-18 19:00:57 +0530

    config updated

--- main.tf
+++ main.tf
@@ -1509,22 +1509,22 @@ module "firewall_policy" {
   depends_on                 = [module.azure_firewall]
 }
 
-module "ip_group" {
-  source                = "app.terraform.io/Motifworks/private_endpoint/azurerm"
-  version               = "1.0.0"
-  resource_group_output = module.resource_Group.resource_group_output
+# module "ip_group" {
+#   source                = "app.terraform.io/Motifworks/private_endpoint/azurerm"
+#   version               = "1.0.0"
+#   resource_group_output = module.resource_Group.resource_group_output
 
-  ip_group_list = [
-    {
-      ip_group_name       = "ddi-ip-group-1"
-      resource_group_name = "rg-ddi-dev1"
-      location            = "westus"
-      cidrs               = ["10.0.0.0/24", "192.168.0.0/24"]
-      tags = {
-        environment = "Prod"
-      }
-  }]
-}
+#   ip_group_list = [
+#     {
+#       ip_group_name       = "ddi-ip-group-1"
+#       resource_group_name = "rg-ddi-dev1"
+#       location            = "westus"
+#       cidrs               = ["10.0.0.0/24", "192.168.0.0/24"]
+#       tags = {
+#         environment = "Prod"
+#       }
+#   }]
+# }
 
 # # module "application_gateway" {
 # #   source  = "app.terraform.io/Motifworks/application_gateway/azurerm"
