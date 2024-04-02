aa69f9eb797f92ab497aab49ae4a2db9e3f8f897
sunanda sharma <sunandasharma@ddiworld.com>
2024-04-02 18:50:31 +0530

    config updated

--- output.tf
+++ output.tf
@@ -10,13 +10,13 @@ output "service_endpoint_policy_output" {
   value = module.service_endpoint_policy.service_endpoint_policy_output
 }
 
-# output "vnet_subnet_output" {
-#   value = module.subnet.vnet_subnet_output
-# }
+output "vnet_subnet_output" {
+  value = module.subnet.vnet_subnet_output
+}
 
-# output "snet_network_security_group_output" {
-#   value = module.network_security_group.network_security_group_output
-# }
+output "snet_network_security_group_output" {
+  value = module.network_security_group.network_security_group_output
+}
 
 # output "route_table_output" {
 #   value = module.route_table.route_table_output
@@ -26,23 +26,23 @@ output "service_endpoint_policy_output" {
 #   value = module.virtual_network_dns.virtual_network_dns_output
 # }
 
-output "public_ip_output" {
-  value = module.public_ip.public_ip_output
-}
+# output "public_ip_output" {
+#   value = module.public_ip.public_ip_output
+# }
 
 # output "network_interface_card_output" {
 #   value = module.network_interface_card.network_interface_card_output
 # }
 
-output "azure_firewall_output" {
-  value = module.azure_firewall.azure_firewall_output
-}
+# output "azure_firewall_output" {
+#   value = module.azure_firewall.azure_firewall_output
+# }
 
 # output "load_balancer_output" {
 #   value = module.load_balancer.load_balancer_output
 # }
 
-output "traffic_manager_profile_output" {
-  value = module.traffic_manager_profile.traffic_manager_profile_output
-}
+# output "traffic_manager_profile_output" {
+#   value = module.traffic_manager_profile.traffic_manager_profile_output
+# }
 
